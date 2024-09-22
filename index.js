8460a71e86f833d1a26b83bfd75c6c4777590e64
Joao-VictorCm <joaovictor27cm@gmail.com>
2024-09-22 20:17:00 -0300

    add função que verifica se a senha esta correta

--- index.js
+++ index.js
@@ -2,17 +2,52 @@
 //Certifique-se de ter instalado todas as dependências com "npm i".
 //A senha é ILoveProgramming
 
-import express from "express"
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+import bodyParser from "body-parser";
 
+const app = express();
+const port = 3001;
 
-const app = express()
-const port = 3000
+var userIsAuthorised = false;
+// Obter o diretório corretamente no ES Modules
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
+// Middleware para servir arquivos estáticos
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", (req, res) =>{
-    res.send("<h1>Hola</h1>")
-})
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen(port, () =>{
-    console.log(`Servidor funcionando na porta:${port}`)
-})
+function acesso(req, res, next) {
+  const password = req.body["password"];
+  if (password === "teste") {
+    userIsAuthorised = true;
+  }
+  next();
+}
+
+app.use(acesso);
+
+// Servir o arquivo index.html
+app.get("/", (req, res) => {
+  res.setHeader("Content-Type", "text/html"); // Força o tipo MIME para HTML
+  res.removeHeader("Content-Disposition"); // Remove cabeçalhos que forçam download
+  res.sendFile(path.join(__dirname, "public", "page.html"));
+});
+
+app.post("/check", (req, res) => {
+  if (userIsAuthorised) {
+    res.setHeader("Content-Type", "text/html"); 
+    res.removeHeader("Content-Disposition");
+    res.sendFile(path.join(__dirname, "public", "acesso.html"));
+  } else {
+    res.setHeader("Content-Type", "text/html"); 
+    res.removeHeader("Content-Disposition");
+    res.sendFile(path.join(__dirname, "public", "page.html"));
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
